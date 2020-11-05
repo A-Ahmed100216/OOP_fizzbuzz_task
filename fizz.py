3d25e317d3d0bd0a67b2de53d5e4292a6a4cf744
Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
2020-11-05 16:03:44 +0000

    completed fizzbuzz core task

--- fizz.py
+++ fizz.py
@@ -1,34 +1,27 @@
-# Create a class called Fizz
-class FizzBuzz():
+# Create a class called FizzBuzz
+class FizzBuzz:
 
     def __init__(self):
-        self.number= 10
-        self.start=0
-        self.end=10
+        # Create a number attribute
+        self.number=[i for i in range(1,101)]
 
-
-    def fizz(self):
-        for i in range(self.start,self.end):
-            if i %3==0:
-                return "Fizz"
+    # Create a function
+    def fizzbuzz(self):
+        # For each value in the number attribute range...
+        for n in range(len(self.number)):
+            # If the number is divisible by 5 and 3, prints FizzBuzz
+            if n%3==0 and n%5==0:
+                print("FizzBuzz")
+            # If the number is divisible by 3, prints Fizz
+            elif n%3==0:
+                print("Fizz")
+            # If the number is divisible by 5, prints Buzz
+            elif n%5==0:
+                print("Buzz")
+            # Otherwise, it just prints the number
             else:
-                return i
+                print(n)
 
-#     def buzz(self):
-#         if self.number%5==0:
-#             return "Buzz"
-#         else:
-#             return self.number
-#
-#     def fizzbuzz(self):
-#         if self.number%3==0 and self.number%5==0:
-#             return "FizzBuzz"
-
-
-fizzy=FizzBuzz()
-print(fizzy.fizz())
-# print(fizzy.buzz())
-# * Write a program that prints the numbers from 1 to 100.
-# * For multiples of three print "Fizz" instead of the number
-# * For the multiples of five print "Buzz" instead of the number
-# * For numbers which are multiples of both three and five print "FizzBuzz"
+# Create an instance to test.
+test= FizzBuzz()
+print(test.fizzbuzz())
